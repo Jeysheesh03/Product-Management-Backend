0a8d431968c74c672fbf73883ecb3abe1945f756
Erudite098 <macalde.kerzania1029@gmail.com>
2024-10-19 00:43:08 +0800

    initital commit

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //Malabanan
+            'barcode' => fake()->numberBetween(100000, 999999),
+            'product_name' => fake()->name(),
+            'description' => fake()->text(100),
+            'price' => fake()->numberBetween(100, 1000),
+            'quantity' => fake()->numberBetween(1, 100),
+            'category' => fake()->word(),
+
+
+        ];
+    }
+}
